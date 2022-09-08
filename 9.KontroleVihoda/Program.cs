cd0955fce07a6bcc115be27399e2daa8e6dcf3ba
Baron Von Cat <110116982+BaronVonCat@users.noreply.github.com>
2022-09-08 13:46:41 +0300

    Add files via upload

---
+++ 9.KontroleVihoda/Program.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9.KontroleVihoda
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text;
+            int numberRepetitionsText;
+            string exitTheLoop = "exit";
+            string userInput;
+
+            Console.Write("Введите текст: ");
+            text = Console.ReadLine();
+            Console.Clear();
+            Console.Write("Введите количество повторений: ");
+            numberRepetitionsText = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+            Console.WriteLine("Чтобы выйти из программы введите - " + exitTheLoop);
+            Console.ReadLine();
+
+            while (numberRepetitionsText > 0)
+            {
+                Console.WriteLine(text);
+                numberRepetitionsText--;
+                userInput = Console.ReadLine();
+                
+                if (userInput == exitTheLoop)
+                {
+                    Console.WriteLine("Вы вышли из программы");
+                    break;
+                }
+
+            }
+        }
+    }
+}
