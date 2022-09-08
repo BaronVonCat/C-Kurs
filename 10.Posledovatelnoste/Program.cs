f248d02bc3489de7ba78140c27d97a9491ea72da
Baron Von Cat <110116982+BaronVonCat@users.noreply.github.com>
2022-09-08 14:21:32 +0300

    Add files via upload

---
+++ 10.Posledovatelnoste/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10.Posledovatelnoste
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int beginningSequence = 5, endSequence = 96, increasingSequence = 7;
+                beginningSequence <= endSequence; beginningSequence += increasingSequence)
+            {
+                Console.WriteLine(beginningSequence);
+            }
+        }
+    }
+}
