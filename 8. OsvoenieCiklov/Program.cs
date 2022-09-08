a0b0ce79e9150e13e998b24d476401dafbda06f4
Baron Von Cat <110116982+BaronVonCat@users.noreply.github.com>
2022-09-08 12:58:22 +0300

    Add files via upload

---
+++ 8. OsvoenieCiklov/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8.OsvoenieCiklov
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text;
+            int numberRepetitionsText;
+
+            Console.Write("Введите текст: ");
+            text = Console.ReadLine();
+            Console.Clear();
+            Console.Write("Введите количество повторений: ");
+            numberRepetitionsText = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+            while (numberRepetitionsText > 0) 
+            {
+                Console.WriteLine(text);
+                numberRepetitionsText--;
+            }
+        }
+    }
+}
