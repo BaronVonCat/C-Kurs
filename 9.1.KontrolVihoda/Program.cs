5b928d8cda0d45133dfb3a267d640460f285f7e1
Baron Von Cat <110116982+BaronVonCat@users.noreply.github.com>
2022-09-09 13:47:27 +0300

    Add files via upload

---
+++ 9.1.KontrolVihoda/Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9._1.KontrolVihoda
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string exitProgram = "exit";
+            string userInput;
+            userInput = Console.ReadLine();
+
+            while (userInput != exitProgram)
+            {
+                Console.WriteLine("Вы сделали шаг");
+                userInput = Console.ReadLine();
+            }
+        }
+    }
+}
