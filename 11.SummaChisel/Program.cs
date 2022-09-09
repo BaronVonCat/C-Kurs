974e0b6d294f832b7dfae8d2b9033452dd6887e6
Baron Von Cat <110116982+BaronVonCat@users.noreply.github.com>
2022-09-09 14:59:43 +0300

    Add files via upload

---
+++ 11.SummaChisel/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11.SummaChisel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int number = rand.Next(101);
+            int sum = 0;
+
+            Console.WriteLine(number);
+
+            for (int i = 1; i <= number; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    sum += i;
+                }
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
