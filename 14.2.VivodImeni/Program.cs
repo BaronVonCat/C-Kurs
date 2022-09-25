3b319807106a43e82248fae9c658246810bd6fa4
Baron Von Cat <110116982+BaronVonCat@users.noreply.github.com>
2022-09-25 13:15:29 +0300

    Add files via upload

--- 14.2.VivodImeni/Program.cs
+++ 14.2.VivodImeni/Program.cs
@@ -10,40 +10,26 @@ namespace _14._2.VivodImeni
     {
         static void Main(string[] args)
         {
-            string lineSymbols;
             string userName;
             char userSymbol;
             int numberSymbols;
             string lineName;
+            string lineSymbols;
 
             Console.Write("Введите ваше имя: ");
             userName = Console.ReadLine();
             Console.Write("Введите символ: ");
             userSymbol = Convert.ToChar(Console.ReadLine());
             numberSymbols = userName.Length;
+            lineSymbols = userSymbol.ToString();
             lineName = userSymbol + userName + userSymbol;
 
-            for (int i = numberSymbols; i != -2; i--)
+            for (int i = numberSymbols; i != -1; i--)
             {
-                Console.Write(userSymbol);
-
-                if (i == -1)
-                {
-                    Console.WriteLine();
-                }
+                lineSymbols += userSymbol;
             }
 
-            Console.WriteLine(lineName);
-
-            for (int i = numberSymbols; i != -2; i--)
-            {
-                Console.Write(userSymbol); 
-
-                if (i == -1)
-                {
-                    Console.WriteLine();
-                }
-            }
+            Console.WriteLine(lineSymbols + "\n" + lineName + "\n" + lineSymbols);
         }
     }
 }
