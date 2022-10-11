c73b523d5f7ba4b83f5c1868856a1870951ac319
Baron Von Cat <110116982+BaronVonCat@users.noreply.github.com>
2022-10-11 14:51:56 +0300

    Add files via upload

---
+++ 3.Massivi/3.2.NaibolshiyElement/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace _3._2.NaibolshiyElement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random =new Random();
+            int[,] numbers = new int[10, 10];
+            int numbersMax = 1000;
+            int numbersMin = 1;
+            int largestNumber = 0;
+
+            for (int i = 0; i < numbers.GetLength(0); i++ )
+            {
+                for (int j = 0; j < numbers.GetLength(1); j++)
+                {
+                    numbers[i, j] = random.Next(numbersMin, numbersMax);
+                    Console.Write(numbers[i, j] + "  ");
+                }
+
+                Console.WriteLine();
+            }
+
+            foreach (int i in numbers)
+            {
+                if (i > largestNumber)
+                {
+                    largestNumber = i;
+                }
+            }
+            
+            Console.WriteLine("\nНаибольшее число - " + largestNumber);
+
+            for (int i = 0; i < numbers.GetLength(0); i++)
+            {
+                for (int j = 0; j < numbers.GetLength(1); j++)
+                {
+                    if (numbers[i, j] == largestNumber)
+                    {
+                        numbers[i, j] = 0;
+                    }
+                }
+            }
+
+            Console.WriteLine();
+
+            for (int i = 0; i < numbers.GetLength(0); i++)
+            {
+                for (int j = 0; j < numbers.GetLength(1); j++)
+                {
+                    Console.Write(numbers[i, j] + "  ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
