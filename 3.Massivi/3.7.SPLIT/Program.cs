14e5633e8dfbc18bbc456e5f2861ea05c0450925
Baron Von Cat <110116982+BaronVonCat@users.noreply.github.com>
2022-11-04 16:43:20 +0300

    Add files via upload

---
+++ 3.Massivi/3.7.SPLIT/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3._7.SPLIT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "Дана строка с текстом";
+            string[] words = text.Split(' ');
+
+            Console.WriteLine(text + "\n");
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                Console.WriteLine(words[i]);
+            }
+        }
+    }
+}
