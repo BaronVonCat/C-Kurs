aa8628519eae3f1a970a6561c392e911590fc6d8
Baron Von Cat <110116982+BaronVonCat@users.noreply.github.com>
2022-11-04 16:04:54 +0300

    Add files via upload

--- 3.Massivi/3.5.PodmassivPovtoreniaChisel/Program.cs
+++ 3.Massivi/3.5.PodmassivPovtoreniaChisel/Program.cs
@@ -13,41 +13,41 @@ namespace _3._5.PodmassivPovtoreniaChisel
             Random random = new Random();
             int[] numbers = new int[30];
             int numberMin = 0;
-            int numberMax = 6;
-            int sum = 0;
-            int consecutiveNumbers = 1;
+            int numberMax = 2;
+            int numericalSequence = 1;
+            int numericalSequenceMax = 1;
             int maximumConsecutiveNumber = 0;
 
             for (int i = 0; i < numbers.Length; i++)
             {
                 numbers[i] = random.Next(numberMin, numberMax);
                 Console.Write(numbers[i] + " ");
+            }
 
-                if (i == 0)
-                {
-                    sum++;
-                    maximumConsecutiveNumber = numbers[i];
-                }
-                else if (numbers[i] == numbers[i - 1])
-                {
-                    sum++;
+            maximumConsecutiveNumber = numbers[0];
 
-                    if (sum > consecutiveNumbers)
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                if (numbers[i] == numbers[i - 1])
+                {
+                    numericalSequence++;
+
+                    if (numericalSequence > numericalSequenceMax)
                     {
-                        consecutiveNumbers = sum;
+                        numericalSequenceMax = numericalSequence;
                         maximumConsecutiveNumber = numbers[i];
                     }
                 }
                 else
                 {
-                    sum = 1;
+                    numericalSequence = 1;
                 }
             }
 
             Console.WriteLine("\nЧисло с максимальной последовательностю чисел - "
                 + maximumConsecutiveNumber +
-                "\nМаксимальная последовательность - " + 
-                consecutiveNumbers);
+                "\nМаксимальная последовательность - " +
+                numericalSequenceMax);
         }
     }
 }
