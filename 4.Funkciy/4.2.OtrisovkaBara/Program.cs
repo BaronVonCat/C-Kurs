b980b99805d6c3d6cf725ffeabc4968a37da181f
Baron Von Cat <110116982+BaronVonCat@users.noreply.github.com>
2023-01-07 05:17:35 +0300

    Add files via upload

---
+++ 4.Funkciy/4.2.OtrisovkaBara/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4._2.OtrisovkaBara
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int healthPointLimit = 100;
+            int healthPointMin = 0;
+            int healthPointMax = healthPointLimit + 1;
+            int healthPoint = random.Next(healthPointMin, healthPointMax);
+
+            Console.WriteLine(healthPoint);
+            RenderinghealthPointBar(healthPoint, healthPointLimit);
+        }
+
+        static void RenderinghealthPointBar(int healthPoint, int healthPointLimit)
+        {
+            double lenghtBar = 200;
+
+            Console.Write("|");
+
+            for (int i = 0; i < lenghtBar; i++)
+            {
+                if (healthPointLimit / lenghtBar * i < healthPoint)
+                {
+                    Console.Write("+");
+                }
+                else
+                {
+                    Console.Write("-");
+                }
+            }
+
+            Console.WriteLine("|");
+        }
+    }
+}
