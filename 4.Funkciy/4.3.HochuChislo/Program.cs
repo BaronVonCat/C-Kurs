4df96f19f3780173ade93597bdb92341cca39b89
Baron Von Cat <110116982+BaronVonCat@users.noreply.github.com>
2023-01-21 04:18:59 +0300

    Add files via upload

---
+++ 4.Funkciy/4.3.HochuChislo/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4._3.HochuChislo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int userNumber;
+
+            userNumber = ToRequestNumber();
+        }
+
+        static int ToRequestNumber()
+        {
+            bool hasCheckCompletedSuccessfully = false;
+            int number = int.MinValue;
+
+            while (hasCheckCompletedSuccessfully == false)
+            {
+                bool isNumber;
+                string userInput;
+
+                Console.WriteLine("Хочу число: ");
+                userInput = Console.ReadLine();
+                isNumber = int.TryParse(userInput, out number);
+
+                if (isNumber == true)
+                {
+                    hasCheckCompletedSuccessfully = true;
+                }
+                else
+                {
+                    Console.WriteLine("Кожаный, мне нужно число, а ты что ввёл?! Давай по новой.");
+                }
+            }
+
+            return number;
+        }
+    }
+}
