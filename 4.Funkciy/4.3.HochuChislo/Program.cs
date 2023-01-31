0a4fe3363b7544f040a7c41db881ec8e0bcf806f
Baron Von Cat <110116982+BaronVonCat@users.noreply.github.com>
2023-01-31 15:01:27 +0300

    Add files via upload

---
+++ 4.Funkciy/4.3.HochuChislo/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _4._3.HochuChislo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int userNumber;
+
+            userNumber = GetNumber();
+            Console.WriteLine($"Ваше число - {userNumber}");
+        }
+
+        static int GetNumber()
+        {
+            bool hasNumberBeenReceived = false;
+            string userInput;
+            int number = 0;
+
+            while (hasNumberBeenReceived == false)
+            {
+                Console.Write("Хочу число: ");
+                userInput = Console.ReadLine();
+                Console.Clear();
+                hasNumberBeenReceived = int.TryParse(userInput, out number);
+
+                if (hasNumberBeenReceived == false)
+                {
+                    Console.WriteLine("Кожаный, мне нужно число, а ты что ввёл?! Давай по новой.");
+                }
+            }
+
+            return number;
+        }
+    }
+}
