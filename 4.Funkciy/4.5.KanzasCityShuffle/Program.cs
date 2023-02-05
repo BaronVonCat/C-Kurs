f1a37650573a923ab4fe8ee6193f29c9d99c0f45
Baron Von Cat <110116982+BaronVonCat@users.noreply.github.com>
2023-02-05 13:53:22 +0300

    Add files via upload

---
+++ 4.Funkciy/4.5.KanzasCityShuffle/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4._5.KanzasCityShuffle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = СreateNumbers();
+
+            DrawNumbers(numbers);
+            Console.WriteLine();
+            numbers = ShuffleNumbers(numbers);
+            DrawNumbers(numbers);
+            Console.WriteLine();
+            numbers = ShuffleNumbers(numbers);
+            DrawNumbers(numbers);
+            Console.WriteLine();
+        }
+
+        static int[] СreateNumbers()
+        {
+            int lenghtNumbers = 10;
+            int[] numbers = new int[lenghtNumbers];
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = i;
+            }
+            return numbers;
+        }
+
+        static void DrawNumbers(int[] numbers)
+        {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write(numbers[i]);
+            }
+        }
+
+        static int[] ShuffleNumbers(int[] numbers)
+        {
+            Random random= new Random();
+            int shuffleNumber;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                shuffleNumber = random.Next(0, numbers.Length);
+                (numbers[i], numbers[shuffleNumber]) = (numbers[shuffleNumber], numbers[i]); 
+            }
+
+            return numbers;
+        }
+    }
+}
