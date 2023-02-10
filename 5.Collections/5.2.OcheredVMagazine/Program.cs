06f380c27d537153954d4cda2d2d1a0c86b0fcf9
Baron Von Cat <110116982+BaronVonCat@users.noreply.github.com>
2023-02-10 22:41:01 +0300

    Add files via upload

---
+++ 5.Collections/5.2.OcheredVMagazine/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5._2.OcheredVMagazine
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> buyers = CreateBuyers();
+        }
+
+        static Queue<int> CreateBuyers()
+        {
+            int numberBuyers = 10;
+            Queue<int> buyers = new Queue<int>();
+
+            for (int i = 0; i < )
+            {
+
+            }
+
+            return buyers;
+        }
+    }
+}
