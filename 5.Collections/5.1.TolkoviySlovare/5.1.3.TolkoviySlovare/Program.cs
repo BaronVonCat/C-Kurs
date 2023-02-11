50ad0b6ed2f8aab9c79e0fd6b45e5011e0bf3474
Baron Von Cat <110116982+BaronVonCat@users.noreply.github.com>
2023-02-11 23:53:16 +0300

    Add files via upload

---
+++ 5.Collections/5.1.TolkoviySlovare/5.1.3.TolkoviySlovare/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Security.Cryptography;
+using System.Globalization;
+
+namespace _5._1.TolkoviySlovare
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> words = CreateWords();
+            bool isUserExit = false;
+
+            while (isUserExit == false)
+            {
+                const string CommandExit = "0";
+
+                string userinput;
+                string manual = "Введите номер команды или искомое слово: ";
+
+                Console.WriteLine($"{manual}\n\n{CommandExit}. Выход из программы");
+                Console.SetCursorPosition( manual.Length, 0 );
+                userinput = Console.ReadLine();
+                Console.Clear();
+
+                if (userinput != CommandExit)
+                {
+                    SearchMeaningWord(words, userinput);
+                }
+                else
+                {
+                    isUserExit = true;
+                }
+            }
+        }
+
+        static Dictionary<string, string> CreateWords()
+        {
+            Dictionary<string, string> words = new Dictionary<string, string>();
+            string[] fileWords = 
+                { "Врач-человек, что предотворощает болезни и недуги.",
+                "Архитектор-человек, что проектирует здания и сооружения.",
+                "Велосипед-средство передвижения на двух колёсах."};
+
+            for (int i = 0; i < fileWords.Length; i++)
+            {
+                string[] wordAndValue = fileWords[i].Split('-');
+                string word = wordAndValue[0];
+                string valueWord = wordAndValue[1];
+                words.Add(word, valueWord);
+            }
+
+            return words;
+        }
+
+        static void SearchMeaningWord(Dictionary<string, string> words, string searchWord)
+        {
+            TextInfo textInfo = CultureInfo.CurrentCulture.TextInfo;
+
+            searchWord = searchWord.ToLower();
+            searchWord = textInfo.ToTitleCase(searchWord);
+
+            if (words.ContainsKey(searchWord))
+            {
+                Console.WriteLine($"{searchWord} - {words[searchWord]}");
+            }
+            else
+            {
+                Console.WriteLine("Некорректный запрос!");
+            }
+
+            Console.ReadKey();
+            Console.Clear();
+        }
+    }
+}
