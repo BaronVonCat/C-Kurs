bdf9887519b16302c5f88d297b73a61d6417ff57
Baron Von Cat <110116982+BaronVonCat@users.noreply.github.com>
2023-03-04 01:04:39 +0300

    Add files via upload

---
+++ 6.OOP/6.1.RabotaSKlassami/6.1.0.RabotaSKlassami/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6._1.RabotaSKlassami
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player = new Player("Бекташи", "Снайпер", 1);
+
+            player.ShowInfo();
+        }
+    }
+
+    class Player
+    {
+        private string _name;
+        private string _grade;
+        private int _level;
+
+        public Player(string name, string grade, int level)
+        {
+            _name = name;
+            _grade = grade;
+            _level = level;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Данные об игроке:\n\n" +
+                $"Имя - {_name}\n" +
+                $"Класс - {_grade}\n" +
+                $"Уровень - {_level}");
+        }
+    }
+}
---
+++ 6.OOP/6.1.RabotaSKlassami/6.1.0.RabotaSKlassami/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("6.1.RabotaSKlassami")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("6.1.RabotaSKlassami")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("bc390b6c-37d5-4866-b6f5-c7a264f76d56")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
