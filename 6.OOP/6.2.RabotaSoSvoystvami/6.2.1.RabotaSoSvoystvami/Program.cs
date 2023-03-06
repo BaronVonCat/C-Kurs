ffd67274f7f6f386e20e82bea9fa6fd30680140f
Baron Von Cat <110116982+BaronVonCat@users.noreply.github.com>
2023-03-06 21:49:46 +0300

    Add files via upload

---
+++ 6.OOP/6.2.RabotaSoSvoystvami/6.2.1.RabotaSoSvoystvami/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _6._2.RabotaSoSvoystvami
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player = new Player('O', 5, 10);
+            Renderer renderer = new Renderer();
+
+            renderer.DrawPlayer(player);
+            Console.CursorVisible = false;
+            Console.ReadKey();
+        }
+    }
+
+    class Player
+    {
+        public Player(char sign, int positionX, int positionY)
+        {
+            Sign = sign;
+            X = positionX;
+            Y = positionY;
+        }
+
+        public char Sign { get; private set; }
+        public int X { get; private set; }
+        public int Y { get; private set; }
+    }
+
+    class Renderer
+    {
+        public void DrawPlayer(Player player)
+        {
+            Console.SetCursorPosition(player.X, player.Y);
+            Console.Write(player.Sign);
+        }
+    }
+}
