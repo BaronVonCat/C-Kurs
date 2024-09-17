e240ecc4b681a01728dcec94d26e0d8c25114fc9
Baron Von Cat <110116982+BaronVonCat@users.noreply.github.com>
2024-09-17 12:22:46 +0300

    Add files via upload

---
+++ 7.Linq/7.07.ObjedineniyeVoiysk/7.07.00.ObjedineniyeVoiysk/Program.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _7._07.ObjedineniyeVoiysk
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SoldierCreator creator = new SoldierCreator();
+            List<Soldier> soldiersA = creator.CreateSoldiers();
+            List<Soldier> soldiersB = creator.CreateSoldiers();
+            List<Soldier> transferSoldiers = soldiersA.Where(soldier => soldier.Name.StartsWith("Б")).ToList();
+
+            Console.WriteLine("До перевода:");
+            Console.WriteLine("Отряд А");
+            ShowSoldiers(soldiersA);
+            Console.WriteLine();
+            Console.WriteLine("Отряд Б");
+            ShowSoldiers(soldiersB);
+            Console.WriteLine();
+            soldiersB = soldiersB.Union(transferSoldiers).ToList();
+            soldiersA = soldiersA.Except(transferSoldiers).ToList();
+            Console.WriteLine("После перевода:");
+            Console.WriteLine("Отряд А");
+            ShowSoldiers(soldiersA);
+            Console.WriteLine();
+            Console.WriteLine("Отряд Б");
+            ShowSoldiers(soldiersB);
+            Console.ReadKey();
+        }
+
+        static void ShowSoldiers(List<Soldier> soldiers)
+        {
+            foreach (Soldier soldier in soldiers)
+            {
+                soldier.ShowInfo();
+                Console.WriteLine();
+            }
+        }
+    }
+
+    class Soldier
+    {
+        public Soldier(string name, string rank)
+        {
+            Name = name;
+            Rank = rank;
+        }
+
+        public string Name { get; private set; }
+        public string Rank { get; private set; }
+
+        public void ShowInfo()
+        {
+            Console.Write($"{Name} | {Rank}");
+        }
+    }
+
+    class SoldierCreator
+    {
+        private NamesCreator _namesCreator;
+
+        public SoldierCreator()
+        {
+            _namesCreator = new NamesCreator();
+        }
+
+        public List<Soldier> CreateSoldiers(int numberSoldiers = 10)
+        {
+            List<Soldier> soldiers = new List<Soldier>();
+
+            for (int i = 0; i < numberSoldiers; i++)
+            {
+                soldiers.Add(CreateSoldier());
+            }
+
+            return soldiers;
+        }
+
+        private Soldier CreateSoldier()
+        {
+            string name = _namesCreator.GenerateFullName();
+            string rank = GenerateRank();
+
+            return new Soldier(name, rank);
+        }
+
+        private string GenerateRank()
+        {
+            List<string> ranks = new List<string>
+            {
+                "Рядовой",
+                "Ефврейтор",
+                "Сержант",
+                "Прапорщик",
+                "Лейтенант",
+                "Капитан"
+            };
+
+            return ranks[UserUtils.GenereteRandom(ranks.Count)];
+        }
+    }
+
+    class NamesCreator
+    {
+        public string GenerateFullName()
+        {
+            return $"{GenerateSurnames()} {GenerateNames()} {GeneratePatronymics()}";
+        }
+
+        private string GenerateNames()
+        {
+            List<string> names = new List<string>
+            {
+                "Дмитрий",
+                "Иван",
+                "Сергей",
+                "Антон",
+                "Роман",
+                "Артём",
+                "Юрий",
+                "Олег",
+                "Святослав",
+                "Марк"
+            };
+
+            return names[UserUtils.GenereteRandom(names.Count)];
+        }
+
+        private string GenerateSurnames()
+        {
+            List<string> surnames = new List<string>
+            {
+                "Ушаков",
+                "Кутузов",
+                "Белоцерковский",
+                "Барабанов",
+                "Лаптев",
+                "Горелов",
+                "Ковалёв",
+                "Козлов",
+                "Давлятов",
+                "Суворов"
+            };
+
+            return surnames[UserUtils.GenereteRandom(surnames.Count)];
+        }
+
+        private string GeneratePatronymics()
+        {
+            List<string> patronymics = new List<string>
+            {
+                "Сергеевич",
+                "Олегович",
+                "Валерьевич",
+                "Святославович",
+                "Антонович",
+                "Дмитриевич",
+                "Семёнович",
+                "Юрьевич",
+                "Маркович",
+                "Алексеевич"
+            };
+
+            return patronymics[UserUtils.GenereteRandom(patronymics.Count)];
+        }
+    }
+
+    class UserUtils
+    {
+        private static Random s_random;
+
+        static UserUtils()
+        {
+            s_random = new Random();
+        }
+
+        public static int GenereteRandom(int minValue, int maxValue)
+        {
+            int randomNumber = s_random.Next(minValue, maxValue);
+            return randomNumber;
+        }
+
+        public static int GenereteRandom(int maxValue)
+        {
+            return s_random.Next(maxValue);
+        }
+    }
+}
---
+++ 7.Linq/7.07.ObjedineniyeVoiysk/7.07.00.ObjedineniyeVoiysk/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("7.07.ObjedineniyeVoiysk")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("7.07.ObjedineniyeVoiysk")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("4a9cd8b8-981b-4528-8cb4-e45d8a204623")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
